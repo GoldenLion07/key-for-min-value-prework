2169d48bd33f947b2b598c60278234cfd3288bb6
Scott M <gmat191@gmail.com>
2018-10-11 01:11:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   low_key = nil 
   low_value = nil 
   name_hash.each do |k, v|
-    if low_value == nil || v < l
+    if low_value == nil || v < low_value
+      
 
 end
